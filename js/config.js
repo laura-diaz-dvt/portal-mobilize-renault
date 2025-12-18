f458ea2de486ba34494238878eba677440f0c881
Laura Díaz <laura.diaz@devoteam.com>
2025-12-18 12:00:49 +0100

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,10 @@
+export const DASHBOARDS = [
+{ id: 5959, title: "Bienvenida/o al Portal Mobilize" },
+{ id: 5978, title: "Ventas" },
+{ id: 5661, title: "Informe Comercial" },
+{ id: 5909, title: "Producción detallada financiación" }
+]
+
+
+export const LOOKER_ORIGIN = "https://renaultssadev.cloud.looker.com"
+export const EMBED_DOMAIN = "https://laura-diaz-dvt.github.io"
